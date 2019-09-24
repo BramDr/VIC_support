2edb481eed1b438967147e7534c6b33bf8ce315d
Bram Droppers <bram.droppers@wur.nl>
2019-09-24 14:32:32 +0200

    added wofost sensitivity analysis scripts

---
+++ Scripts/Generate/wofost/saveConfigSA.R
@@ -0,0 +1,96 @@
+library(ncdf4)
+library(fields)
+rm(list = ls())
+
+# Input
+mask.file <- "../../../Data/Primary/VIC/domain_global.nc"
+force.dir <- "../../../Data/WOFOST/Forcing/global/"
+crop.dir <- "../../../Data/WOFOST/Parameters/Crop/global/"
+management.dir <- "../../../Data/WOFOST/Parameters/Management/global/"
+site.dir <- "../../../Data/WOFOST/Parameters/Site/global/"
+soil.dir <- "../../../Data/WOFOST/Parameters/Soil/global/"
+output.dir <- "../../../Data/WOFOST/Output/global/"
+configuration.param.out <- "../../../Data/WOFOST/Configuration/Parameters/global/"
+configuration.force.out <- "../../../Data/WOFOST/Configuration/Forcing/global/"
+
+# Load
+nc <- nc_open(mask.file)
+mask <- ncvar_get(nc, nc$var$mask)
+lats <- nc$dim$lat$vals
+lons <- nc$dim$lon$vals
+nc_close(nc)
+
+# Setup
+crop.files <- list.files(crop.dir, full.names = T)
+force.files <- list.files(force.dir, full.names = T)
+management.files <- list.files(management.dir, full.names = T)
+site.files <- list.files(site.dir, full.names = T)
+soil.files <- list.files(soil.dir, full.names = T)
+soil.files <- grep(x = soil.files, pattern = "EC3")
+years <- 1979:2016
+
+# Calculate
+for (x in 1:length(lons)) {
+  for (y in 1:length(lats)) {
+    if (is.na(mask[x, y]) || mask[x, y] == 0) {
+      next
+    }
+
+    # Parameters
+    file.out <- paste0(configuration.param.out, "param_config_", lats[y], "N_", lons[x], "E", ".txt")
+    print(basename(file.out))
+
+    desc.out <- paste0(
+      "
+** WOFOST PARAMETER CONFIGURATION FILE for use with WOFOST Version 5.0, June 1990
+** Used for VIC-WOFOST sensitivity analysis
+** Latitude: ", lats[y], " N
+** Longitude: ", lons[x], " E
+      "
+    )
+
+    management.file <- management.files[1]
+    site.file <- grep(x = site.files, pattern = paste0(lats[y], "N_", lons[x], "E"), value = T)
+    soil.file <- soil.files[1]
+    out.file <- paste0(output.dir, "output_", lats[y], "N_", lons[x], "E", ".txt")
+
+    dir.create(dirname(file.out), showWarnings = F, recursive = T)
+    if (file.exists(file.out)) {
+      file.remove(file.out)
+    }
+    writeLines(text = desc.out, con = file.out)
+
+    for (z in 1:length(crop.files)) {
+      crop.file <- crop.files[z]
+      line.out <- paste0(getwd(), "/ ", crop.file, " ", soil.file, " ", management.file, " ", site.file, " 01-03 0 ", out.file)
+
+      write(x = line.out, file = file.out, append = T)
+    }
+
+    # Forcing
+    file.out <- paste0(configuration.force.out, "forcing_config_", lats[y], "N_", lons[x], "E", ".txt")
+    print(basename(file.out))
+
+    desc.out <- paste0(
+      "
+** WOFOST FORCING CONFIGURATION FILE for use with WOFOST Version 5.0, June 1990
+** Used for VIC-WOFOST sensitivity analysis
+** Latitude: ", lats[y], " N
+** Longitude: ", lons[x], " E
+"
+    )
+
+    force.file <- grep(x = force.files, pattern = paste0(lats[y], "N_", lons[x], "E"), value = T)
+
+    dir.create(dirname(file.out), showWarnings = F, recursive = T)
+    if (file.exists(file.out)) {
+      file.remove(file.out)
+    }
+    writeLines(text = desc.out, con = file.out)
+
+    crop.file <- crop.files[z]
+    line.out <- paste0(getwd(), "/", force.file, " ", years[1], " ", years[length(years)], " ", lats[y], " ", lons[x])
+
+    write(x = line.out, file = file.out, append = T)
+  }
+}
